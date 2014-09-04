b145873e262f29ec6bf15446cfd15fd05e7b96b5
yapcheahshen <yapcheahshen@gmail.com>
2014-09-04 19:40:14 +0800

    start adarsha

---
+++ components/adarsha-main/index.jsx
@@ -0,0 +1,43 @@
+/** @jsx React.DOM */
+
+/* to rename the component, change name of ./component.js and  "dependencies" section of ../../component.js */
+var require_kdb=[{ 
+  filename:"jiangkangyur.kdb"  , 
+  url:"http://ya.ksana.tw/kdb/jiangkangyur.kdb" , desc:"jiangkangyur"
+}];  
+//var othercomponent=Require("other"); 
+var bootstrap=Require("bootstrap");  
+var fileinstaller=Require("fileinstaller");
+var kde=Require('ksana-document').kde;  // Ksana Database Engine
+var kse=Require('ksana-document').kse; // Ksana Search Engine (run at client side)
+
+var main = React.createClass({
+  getInitialState: function() {
+    return {dialog:null,res:{},db:null};
+  },
+  onReady:function(usage,quota) {
+    if (!this.state.db) kde.open("jiangkangyur",function(db){
+        this.setState({db:db});
+    },this);      
+    this.setState({dialog:false,quota:quota,usage:usage});
+  },
+  openFileinstaller:function(autoclose) {
+    if (window.location.origin.indexOf("http://127.0.0.1")==0) {
+      require_kdb[0].url=window.location.origin+"/jiangkangyur.kdb";
+    }
+    return <fileinstaller quota="512M" autoclose={autoclose} needed={require_kdb} 
+                     onReady={this.onReady}/>
+  },
+  render: function() {
+    if (!this.state.quota) { // install required db
+        return this.openFileinstaller(true);
+    } else { 
+      return (
+        <div>
+          hello. page count {this.state.db.get("meta").pagecount}
+        </div>
+      );
+    }
+  }
+});
+module.exports=main;

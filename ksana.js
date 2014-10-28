c993b16b6acf687cfc2f93479ae3495420f86e4b
yapcheahshen <yapcheahshen@gmail.com>
2014-10-28 23:48:42 +0800

    change to ksana.js

---
+++ ksana.js
@@ -0,0 +1,40 @@
+jsonp_handler({
+ "version": "1",
+ "build": 70,
+ "title": "Jiang Kangyur",
+ "date": "2014-10-28T11:04:36.296Z",
+ "description": "Jiang kangyur",
+ "files": [
+  "index.html",
+  "build.js",
+  "build.css",
+  "jquery.js",
+  "package.json",
+  "ksana.json",
+  "nodemain.js",
+  "react-with-addons.js",
+  "jiangkangyur.kdb"
+ ],
+ "filedates": [
+  "2014-10-28T10:50:46.000Z",
+  "2014-10-28T10:52:16.000Z",
+  "2014-10-28T10:52:16.000Z",
+  "2014-09-04T10:33:33.000Z",
+  "2014-10-22T01:06:50.000Z",
+  "2014-10-28T10:52:16.000Z",
+  "2014-10-22T01:07:28.000Z",
+  "2014-09-04T10:33:33.000Z",
+  "2014-10-26T03:19:34.000Z"
+ ],
+ "filesizes": [
+  457,
+  470681,
+  124324,
+  277980,
+  379,
+  693,
+  385,
+  671712,
+  131605805
+ ]
+})

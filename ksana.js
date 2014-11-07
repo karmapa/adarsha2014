7838f1008a5724de67c0bb23bda945d002df049c
yap cheah shen <yapcheahshen@gmail.com>
2014-11-07 17:17:08 +0800

    support subfolder in ksana.js

--- ksana.js
+++ ksana.js
@@ -1,8 +1,8 @@
 jsonp_handler({
  "version": "1",
- "build": 285,
+ "build": 288,
  "title": "Jiang Kangyur 加藏文 in ksana.js",
- "date": "2014-11-06T06:13:27.966Z",
+ "date": "2014-11-07T09:10:37.409Z",
  "description": "Jiang kangyur",
  "baseurl": "http://www.dharma-treasure.org/adarsha/",
  "homepage": "http://www.dharma-treasure.org/",
@@ -13,32 +13,32 @@ jsonp_handler({
   "jquery.js",
   "package.json",
   "ksana.js",
-  "banner.png",
   "nodemain.js",
-  "imageicon.png",
+  "banner/imageicon.png",
+  "banner/banner.png",
   "react-with-addons.js",
   "jiangkangyur.kdb"
  ],
  "filedates": [
-  "2014-10-31T02:43:50.000Z",
-  "2014-11-06T06:12:16.000Z",
-  "2014-11-06T06:12:16.000Z",
-  "2014-10-31T02:43:50.000Z",
-  "2014-10-31T02:43:50.000Z",
-  "2014-11-06T06:12:15.000Z",
-  "2014-10-31T02:43:50.000Z",
-  "2014-10-31T02:43:50.000Z",
-  "2014-11-06T05:50:48.000Z"
+  "2014-11-07T08:58:17.000Z",
+  "2014-11-07T09:05:36.000Z",
+  "2014-11-07T09:05:38.000Z",
+  "2014-11-07T08:58:17.000Z",
+  "2014-11-07T08:58:17.000Z",
+  "2014-11-07T09:09:44.000Z",
+  "2014-11-07T08:58:17.000Z",
+  "2014-11-07T08:36:53.000Z",
+  "2014-11-07T09:06:06.000Z"
  ],
  "filesizes": [
-  457,
-  467970,
-  124913,
-  268382,
-  379,
-  867,
-  378,
-  651462,
-  126407778
+  469,
+  6,
+  6,
+  277980,
+  397,
+  863,
+  385,
+  644338,
+  6
  ]
 })
